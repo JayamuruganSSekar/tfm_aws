286ac886f1cf43c2f46517b39d90d0b6da89aa1d
JayamuruganSSekar <47574443+JayamuruganSSekar@users.noreply.github.com>
2019-08-20 09:57:45 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "aws_region" {
   default = "us-east-2"
 }
-
-variable "access_key" {}
-
-variable "secret_key" {}
-
-locals {
-  lambda_zip_path = "${path.module}/mylambda.zip"
+variable "access_key" {
+  default ="AKIATY36TG7ZZHW5GP7O"
+}
+variable "secret_key" { 
+  default = "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"
 }
